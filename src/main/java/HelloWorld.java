bf99b1282998455a8902f0b091edd9e80148d919
Adelina Hubska <adelgub12@gmail.com>
2023-04-24 10:23:51 -0400

    Changed HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,7 +3,8 @@ package main.java;
 public class HelloWorld {
 
     public static void main(String[] args) {
-            System.out.println("Hello GOIT World");
+        System.out.println("Hello GOIT World");
+        System.out.println("This is our second commit");
         }
     }
 
